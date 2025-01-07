71be3cf0f7b528c0506bec5938db188fd4fbb7ce
Kaio Vinícios <kvsgpro@outlook.com>
2025-01-06 21:57:26 -0300

    Add random select of Word and Category

--- SecretWordApp/src/App.jsx
+++ SecretWordApp/src/App.jsx
@@ -4,6 +4,7 @@ import { useCallback, useState, useEffect } from "react";
 import GameOver from "./components/GameOver";
 import Game from "./components/Game";
 import { wordsList } from "./data/words";
+import { use } from "react";
 
 const stages = [
   { id: 1, name: "start" },
@@ -14,12 +15,43 @@ const stages = [
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
-  console.log(words);
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const categorie =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    const word =
+      words[categorie][Math.floor(Math.random() * words[categorie].length)];
+
+    return { word, categorie };
+  };
+
+  const startGame = () => {
+    const { word, categorie } = pickWordAndCategory();
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+    setPickedCategory(categorie)
+    setPickedWord(word)
+    setLetters(letters) // i belive it should be "wordLetters"
+    setGameStage(stages[1].name);
+  };
+
+  const verifyLetter = () => {
+    setGameStage(stages[2].name); // Just a test
+  };
+
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
+
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen />}
-      {gameStage === "game" && <Game />}
-      {gameStage === "end" && <GameOver />}
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
